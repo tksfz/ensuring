df00548a4590d9376a1c4e6ca35951bbfaeadc85
thom <tom@tksfz.org>
2019-10-20 06:29:34 -0700

    Add instanceType subcondition

--- src/main/scala/org/tksf/ensuring/Ensure.scala
+++ src/main/scala/org/tksf/ensuring/Ensure.scala
@@ -19,7 +19,41 @@ case class Except(err: String) extends State[Nothing]
 
 trait Ensure[A] {
   self =>
+
+  /**
+    * Restartable ensure operation
+    */
   def ensure: IO[State[A]]
+
+  def map[B](f: A => B): Ensure[B] = {
+    flatMap(a => Ensure.already(f(a)))
+  }
+
+  def flatMap[B](f: A => Ensure[B]): Ensure[B] = new Ensure[B] {
+    def ensure: IO[State[B]] = {
+      self.ensure.flatMap {
+        case Already(a) => f(a).ensure
+        case ex@Except(err) => IO.delay(ex)
+        case TBD(ioa) => ioaToIOStateB(ioa, f)
+      }
+    }
+
+    private def ioaToIOStateB(ioa: IO[State[A]], f: A => Ensure[B]): IO[State[B]] = {
+      IO.delay(TBD(ioa.flatMap {
+        case Already(a) => f(a).ensure
+        case ex@Except(_) => IO.delay(ex)
+        case TBD(ioa2) => ioaToIOStateB(ioa2, f)
+      }))
+    }
+  }
+
+  /**
+    * Almost like flatTap, but reruns (restarts) this Ensure, on the premise
+    * that the subcondition may affect our output
+    */
+  def subcondition[B](f: A => Ensure[B]): Ensure[A] = {
+    flatMap(a => f(a).flatMap(_ => self))
+  }
 
   def recoverWithDestroyAndRestart(destroy: IO[Unit]): Ensure[A] = new Ensure[A] {
     def ensure: IO[State[A]] = {
@@ -35,30 +69,28 @@ trait Ensure[A] {
     }
   }
 
-  def map[B](f: A => B): Ensure[B] = {
-    flatMap(a => Ensure.already(f(a)))
-  }
-
-  def flatMap[B](f: A => Ensure[B]): Ensure[B] = new Ensure[B] {
-    def ensure: IO[State[B]] = {
-      self.ensure.flatMap {
-        case Already(a) => f(a).ensure
-        case ex@Except(err) => IO.delay(ex)
-        case TBD(ioa) => ioaToIOStateB(ioa, f)
+  def recover(f: IO[A]): Ensure[A] = new Ensure[A] {
+    def ensure: IO[State[A]] = {
+      self.ensure.map {
+        case a@Already(_) => a
+        case tbd@TBD(_) => tbd
+        case Except(err) => TBD(f.map(Already(_)))
       }
-    }
-
-    def ioaToIOStateB(ioa: IO[State[A]], f: A => Ensure[B]): IO[State[B]] = {
-      IO.delay(TBD(ioa.flatMap {
-        case Already(a) => f(a).ensure
-        case ex@Except(_) => IO.delay(ex)
-        case TBD(ioa2) => ioaToIOStateB(ioa2, f)
-      }))
     }
   }
 }
 
 object Ensure {
+  def equal[T](a: => T, b: => T): Ensure[T] = new Ensure[T] {
+    def ensure: IO[State[T]] = IO.delay {
+      if (a == b) {
+        Already(a)
+      } else {
+        Except("not equal")
+      }
+    }
+  }
+
   def already[T](t: T): Ensure[T] = new Ensure[T] {
     def ensure: IO[State[T]] = IO.delay(Already(t))
   }
--- src/main/scala/org/tksf/ensuring/Example.scala
+++ src/main/scala/org/tksf/ensuring/Example.scala
@@ -3,6 +3,10 @@ package org.tksf.ensuring
 import cats.effect.{ExitCode, IO, IOApp}
 import org.tksf.ensuring.ec2.EC2Resources
 import software.amazon.awssdk.services.ec2.Ec2AsyncClient
+import software.amazon.awssdk.services.ec2.model.{AttributeValue, InstanceType, ModifyInstanceAttributeRequest}
+
+import scala.jdk.FutureConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Example extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
@@ -10,6 +14,17 @@ object Example extends IOApp {
     val ec2 = new EC2Resources(ec2Client)
     (for {
       myInstance <- ec2.ec2("i-12345")
+        .subcondition { i =>
+          Ensure.equal(i.instanceType(), InstanceType.M2_XLARGE)
+            .recover { IO.fromFuture { IO {
+              val miar: ModifyInstanceAttributeRequest = ModifyInstanceAttributeRequest.builder()
+                  .instanceType(AttributeValue.builder().value("m2.xlarge").build())
+                  .build()
+              ec2Client.modifyInstanceAttribute(miar).asScala
+                .map(_ => i.instanceType()) // TODO
+            } }
+            }
+        }
     } yield {
       ()
     }).ensure.map { _ =>
