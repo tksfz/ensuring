9539b0c6523521b9dafa68d9dc8c8afa1617d6a1
thom <tom@tksfz.org>
2019-10-20 06:45:05 -0700

    Syntax (eliminate implicit ExecutionContext)

--- src/main/scala/org/tksf/ensuring/Example.scala
+++ src/main/scala/org/tksf/ensuring/Example.scala
@@ -6,7 +6,6 @@ import software.amazon.awssdk.services.ec2.Ec2AsyncClient
 import software.amazon.awssdk.services.ec2.model.{AttributeValue, InstanceType, ModifyInstanceAttributeRequest}
 
 import scala.jdk.FutureConverters._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object Example extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
@@ -16,13 +15,13 @@ object Example extends IOApp {
       myInstance <- ec2.ec2("i-12345")
         .subcondition { i =>
           Ensure.equal(i.instanceType(), InstanceType.M2_XLARGE)
-            .recover { IO.fromFuture { IO {
-              val miar: ModifyInstanceAttributeRequest = ModifyInstanceAttributeRequest.builder()
-                  .instanceType(AttributeValue.builder().value("m2.xlarge").build())
-                  .build()
-              ec2Client.modifyInstanceAttribute(miar).asScala
-                .map(_ => i.instanceType()) // TODO
-            } }
+            .recover {
+              IO.fromFuture(IO {
+                val miar: ModifyInstanceAttributeRequest = ModifyInstanceAttributeRequest.builder()
+                    .instanceType(AttributeValue.builder().value("m2.xlarge").build())
+                    .build()
+                ec2Client.modifyInstanceAttribute(miar).asScala
+              }).map(resp => InstanceType.M2_XLARGE) // TODO fetch the newly provisioned instance type
             }
         }
     } yield {
